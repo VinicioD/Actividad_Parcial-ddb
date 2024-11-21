d440b2e3be7a481a8dbb5df5c9670ef7eecb1f74
VinicioD <afteenoon175@gmail.com>
2024-11-20 21:38:50 -0600

    70%

--- src/main/java/com/example/guiltyspark/database/conneection.java
+++ src/main/java/com/example/guiltyspark/database/conneection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class conneection {
-    private static final String URL = "jdbc:mysql://localhost:3306/recuroso_humanos";
+    private static final String URL = "jdbc:mysql://localhost:3306/GestionEmpleados";
     private static final String USER = "root";
     private static final String PASSWORD = "omega&alpha7";
 
